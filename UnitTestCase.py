f356d8d0580656b14811d67774ec8c581c712eb7
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2019-08-09 11:35:37 +0600

    Complete

--- UnitTestCase.py
+++ UnitTestCase.py
@@ -5,24 +5,25 @@ sampleValues = []
 VariableStem = "X"
 varName = []
 
+
 def takeInputs():
     global NoOfVariables, Boundaries
-    file = open("input.txt", "r")
+    file = open("input1.txt", "r")
     data = file.readlines()
     for i in range(len(data)):
         data[i] = [int(x) for x in data[i].strip().split()]
 
-
     NoOfVariables = data[0][0]
     for i in range(1, len(data)):
         Boundaries.append(data[i])
-    # print(NoOfVariables)
     # print(data)
-    # print(Boundaries)
+    print(NoOfVariables)
+    print(Boundaries)
 
 
 def sampleForOneVariable(lower, upper):
-    values = [lower, upper, lower + 1, upper - 1, lower - 1, upper + 1, (lower + upper) // 2]
+    values = [lower, upper, lower + 1, upper - 1,
+              lower - 1, upper + 1, (lower + upper) // 2]
     return values
 
 
@@ -31,73 +32,76 @@ def chooseSampleValuesFromRange():
         lower, upper = item[0], item[1]
         values = sampleForOneVariable(lower, upper)
         sampleValues.append(values)
-        # print(lower, " ", upper)
-        # print(values)
+
+
+def testNormalValueCase():
+    global NoOfVariables, Boundaries, Labels, varName
+    varName = ["X" + str(i) for i in range(NoOfVariables)]
+    variables, values = "", ""
+    for i in range(NoOfVariables):
+        variables += varName[i] + Labels[6] + ", "
+        values += str(sampleValues[i][6]) + ", "
+    print("[1]", " ", variables.rstrip(", "), "\t", values.rstrip(", "))
+
+
+def testOtherCasesForEachVariable(n):
+    global NoOfVariables, Boundaries, Labels, varName
+    for i in range(0, (n * NoOfVariables)):
+        no = "[" + str(i + 2) + "]"
+        variables, values = "", ""
+        for j in range(NoOfVariables):
+            index = 6
+            if j == i // n:
+                index = i % n
+            variables += varName[j] + Labels[index] + ","
+            values += str(sampleValues[j][index]) + ", "
+        print(no, " ", variables.rstrip(", "), "\t", values.rstrip(", "))
 
 
 def testBoundaryValues():
     print("\n[BOUNDARY VALUE CHECK]\n")
-    global NoOfVariables, Boundaries, Labels, varName
-    varName = ["X"+str(i) for i in range(NoOfVariables)]
-    variables, values = "", ""
-    for i in range(NoOfVariables):
-        variables += varName[i]+Labels[6]+", "
-        values += str(sampleValues[i][6])+", "
-    print("[1]", " ", variables.rstrip(", "), "\t", values.rstrip(", "))
-
-
-    for i in range(0, (4*NoOfVariables)):
-        no = "[" + str(i+2) + "]"
-        variables, values = "", ""
-        for j in range(NoOfVariables):
-            if j == i//4:
-                variables += varName[j]+Labels[i % 4]+", "
-                values += str(sampleValues[j][i % 4])+", "
-            else:
-                variables += varName[j]+Labels[6]+","
-                values += str(sampleValues[j][6])+", "
-        print(no, " ", variables.rstrip(", "), "\t", values.rstrip(", "))
+    testNormalValueCase()
+    testOtherCasesForEachVariable(4)
 
 
 def testRobustly():
     print("\n[ROBUST CHECKING]\n")
-    global NoOfVariables, Boundaries, Labels, varName
-    varName = ["X"+str(i) for i in range(NoOfVariables)]
-    variables, values = "", ""
+    testNormalValueCase()
+    testOtherCasesForEachVariable(6)
 
-    for i in range(NoOfVariables):
-        variables += varName[i]+Labels[6]+", "
-        values += str(sampleValues[i][6])+", "
-    print("[1]", " ", variables.rstrip(", "), "\t", values.rstrip(", "))
-
-
-    for i in range(0, (6*NoOfVariables)):
-        no = "[" + str(i+2) + "]"
-        variables, values = "", ""
-        for j in range(NoOfVariables):
-            if j == i//6:
-                variables += varName[j]+Labels[i % 6]+", "
-                values += str(sampleValues[j][i % 6])+", "
-            else:
-                variables += varName[j]+Labels[6]+","
-                values += str(sampleValues[j][6])+", "
-        print(no, " ", variables.rstrip(", "), "\t", values.rstrip(", "))
 
 def testWorstCases():
-    ""
+    global NoOfVariables, Boundaries, Labels, varName
+    print("\n[WORST-CASE CHECK]\n")
 
+    varName = ["X" + str(i) for i in range(NoOfVariables)]
+    for i in range(0, pow(5, NoOfVariables)):
+        no = "[" + str(i + 1) + "]"
+        variables, values = "", ""
+        res = i
+        indices = []
+        for j in range(NoOfVariables):
+            index = res % 5
+            if index == 4:
+                index += 2
+            variables += varName[j] + Labels[index] + ", "
+            values += str(sampleValues[j][index]) + ", "
+            indices.append(index)
+            res = res // 5
+        print(no, " ", variables.rstrip(", "), "\t\t", values.rstrip(", "))
 
 
 def generateTestCases():
     testBoundaryValues()
     testRobustly()
     testWorstCases()
-    
+
 
 def main():
     takeInputs()
     chooseSampleValuesFromRange()
     generateTestCases()
+
 
 if __name__ == "__main__":
     main()
